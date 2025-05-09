b3bc6ee8107a0fc4ed96e87280f34a2a1410ede0
Tamás Papp <pappt1216@gmail.com>
2025-05-10 00:04:35 +0200

    Partial C++ solution

---
+++ other/ciklus1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+int main() {
+    std::cout << "- 1. feladat\n";
+    for (int i = 10; i --> 0;) if ((10 - i) % 2 == 0) std::cout << (10 - i) << "\n";
+
+    std::cout << "\n- 2. feladat\n";
+    for (int i = 11; i --> 1;) std::cout << i << "\n";
+
+    std::cout << "\n- 3. feladat\n";
+    for (int i = 10; i --> 0;) if (i % 2) std::cout << i << "\n";
+
+    std::cout << "\n- 4. feladat\n";
+    
+    std::string text;
+    std::cout << "Mi legyen a szöveg? ";
+    std::cin >> text;
+
+    int n;
+    std::cout << "Hányszor írja ki a szöveget? ";
+    std::cin >> n; // implicit integer parsing? unsure what's happening in this operator>>
+
+    for (int i = n; i --> 0;) std::cout << text << "\n";
+
+    std::cout << "\n- 5. feladat\n";
+    int m;
+    while (m % 2) {
+        std::cout << "Egy páros szám: ";
+        std::cin >> m;
+    }
+
+    std::cout << "\n- 6. feladat\n";
+    int numThree = 0;
+    for (int i = 20; i --> 0;) {
+        // TODO: how to generate random numbers properly (within an interval) in C++?
+        #define RAND_MAX 20
+        int x = std::rand();
+        if (x % 3 != 0) continue;
+        
+        std::cout << x << "\n";
+        numThree++;
+    }
+    std::cout << "Összesen " << numThree << " darab hárommal osztható szám lett generálva.\n";
+}
